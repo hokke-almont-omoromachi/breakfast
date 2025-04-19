5eef898fdc4b63231be68d9480d27538a2f43a06
hokke-almont-omoromachi <almont.oromomachi.bf@gmail.com>
2025-04-19 12:40:57 +0900

    new pagepage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import BreakfastCheckin from './pages/breakfastCheckin'; // Đảm bảo đường dẫn đúng
-import Guest from './pages/guest'; // Đảm bảo đường dẫn đúng
-import Home from './pages/home'; // Đảm bảo đường dẫn đúng
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import BreakfastCheckin from './pages/breakfastCheckin';
+import Guest from './pages/guest';
+import Home from './pages/home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Routes>
-          {/* Route mặc định điều hướng về /home khi người dùng truy cập vào / */}
           <Route path="/" element={<Navigate to="/home" />} />
           <Route path="/home" element={<Home />} />
           <Route path="/restaurant" element={<BreakfastCheckin />} />
